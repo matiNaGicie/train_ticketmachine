a6ef5aca94e1fef37544dac036798febe7c01609
matien <mnorek@student.agh.edu.pl>
2024-06-17 19:38:25 +0200

    deleted unnecessary build directory

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_TicketMachine_11_FILE C:/Users/PC/Desktop/university/6_sem/oopcpp/biletomat/TicketMachine_11/build/Desktop-Debug/TicketMachine_11.exe)
-set(__QT_DEPLOY_TARGET_TicketMachine_11_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
